d84509a7ca9ab34da14e20482209a3445975c6e1
Reey <trommxxx@mail.ru>
2021-03-08 21:55:26 +0600

    Добавлен детальный просмотр товара

--- hw_48/market/urls.py
+++ hw_48/market/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from market.views import (
-    index_view,
+    index_view, product_view,
 
 )
 
 urlpatterns = [
     path('', index_view, name='product-list'),
+    path('product/<int:pk>', product_view, name='product-view')
 
 ]
--- hw_48/market/views.py
+++ hw_48/market/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from market.models import Product, category_choices
 
+
 def index_view(request):
-    products = Product.objects.filter(balance__gt=0)
+    products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
 
     return render(request, "index.html", {'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    return render(request, 'product_view.html', context={'product': product})
