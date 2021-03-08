d76d25e7cd4eb9133e9475d727e28f3a4385d2d0
Reey <trommxxx@mail.ru>
2021-03-08 19:07:31 +0600

    Модель зарегистрирована в адмике

--- hw_48/market/admin.py
+++ hw_48/market/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from market.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'category', 'balance', 'price']
+    list_filter = ['name', 'category', 'balance', 'price']
+    search_fields = ['name']
+    fields = ['name', 'description', 'category', 'balance', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
