2851e730530ae75665f60249e516118261b54136
Reey <trommxxx@mail.ru>
2021-03-08 21:18:39 +0600

    Добавлено представление главной страницы

--- hw_48/market/views.py
+++ hw_48/market/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from market.models import Product, category_choices
+
+def index_view(request):
+    products = Product.objects.filter(balance__gt=0)
+
+    return render(request, "index.html", {'products': products})
